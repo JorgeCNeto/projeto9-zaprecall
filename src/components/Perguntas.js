76b8298a729fd504b31206386b146cd0d581bf66
Jorge Neto <jorge.c.neto@hotmail.com>
2023-03-06 00:08:41 -0300

    perguntas e questões

--- src/components/Perguntas.js
+++ src/components/Perguntas.js
@@ -1,33 +1,49 @@
 import styled from "styled-components"
 import play from "../assets/seta_play.png"
-import numeroPerguntas from "./numeroPerguntas"
-import { useState } from "react";
-
+import vizualizar from "../assets/seta_virar.png"
+import cards from "./cards"
+import { useEffect, useState } from "react";
 
 
 export default function Perguntas(){
-    const [caixa, setCaixa] = useState("Pergunta")
-  
-    function revelarPergunta(){
-      setCaixa()
-      console.log()
-    }
-
-
     return (
         <BlocoPerguntas>
-            {numeroPerguntas.map((texto) => <Pergunta revelarPergunta={revelarPergunta} texto={`Pergunta ${texto + 1}`}/>)}    
+            {cards.map((texto, index) => <Pergunta index={index} questaoTexto={texto.question} answerTexto={texto.answer}/>)}    
         </BlocoPerguntas>
     )
 }
 
-function Pergunta ({revelarPergunta, texto}) {
-        
+function Pergunta ({revelarPergunta, questaoTexto, answerTexto, index}) {
+    const [questao, setQuestao] = useState(false)
+    const [answer, setAnswer] = useState(false)
+    const [corPadrao, setCorPadrao] = useState("#FFFFFF")
+    const [botao, setBotao] = useState(play)
+    const [flip, setFlip] = useState(false)
+     
+ 
     
+
+    function revelarPergunta(){
+      if (questao === false){
+        setQuestao(true)
+        setCorPadrao("#FFFFD5")
+        setBotao(vizualizar)
+        setFlip(true)
+        
+      } else{
+        setAnswer(true)         
+      }    
+    }
+    
+       
     return (
-        <CaixaPergunta>
-            <TextoPergunta>{texto}</TextoPergunta>
-            <ImagemPergunta src={play} onClick={(event) => revelarPergunta(event)}/>
+        <CaixaPergunta key={index} cor={corPadrao} flip={flip}>
+            <AjusteTexto>
+                <TextoPergunta >{questao ?(answer ? answerTexto : questaoTexto) : `Pergunta ${index + 1}`}</TextoPergunta> 
+            </AjusteTexto>
+            <AjusteImagem>                
+                <ImagemPergunta src={botao} onClick={(event) => revelarPergunta(event)} />                 
+            </AjusteImagem>
         </CaixaPergunta>
     )
 }
@@ -43,7 +59,7 @@ const TextoPergunta = styled.p`
 `
 
 const ImagemPergunta = styled.img`
-    width: 20px;
+    width: ${(props) => props.flip ? "30px" : "20px"};
     height: 23px;
     background: #FFFFFF;
     margin-right: 20px;
@@ -52,9 +68,10 @@ const ImagemPergunta = styled.img`
 const CaixaPergunta = styled.div`
     width: 300px;
     display: flex;
+    flex-direction: ${(props) => props.flip ? "column" : "row"};
     justify-content: space-between;
     align-items: center;
-    background: #FFFFFF;
+    background: ${(props) => props.cor};
     box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
     border-radius: 5px;
     margin-bottom: 25px;
@@ -68,4 +85,16 @@ const BlocoPerguntas = styled.div`
     justify-content: space-between;
     align-items: center;
     
+`
+const AjusteTexto = styled.div`
+    width: 270px;
+    display: flex;
+    justify-content: start;
+`
+
+const AjusteImagem = styled.div`
+    width: 300px;
+    display: flex;
+    justify-content: end;
+    margin-bottom: 6px;
 `
